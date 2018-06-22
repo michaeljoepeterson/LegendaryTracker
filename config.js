0929b1e018e932910b62564f10afae64e08d956c
Michael Peterson <michaeljoepeterson@gmail.com>
2018-06-22 10:33:13 -0600

    test heroku

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:password31@ds261460.mlab.com:61460/legendarydata' || 'mongodb://localhost/legendarydata';
 //exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/legendarydata';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-legendary-app';
-exports.PORT = process.env.PORT;
+exports.PORT = process.env.PORT || 8080;
